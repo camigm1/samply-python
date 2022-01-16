9d25b9a7b0351393ea0af9f4f62f2766ccbc0756
camigm1 <camigm1@gmail.com>
2022-01-15 19:12:25 -0700

    changes

--- cs210/tic-tac-toe.py
+++ cs210/tic-tac-toe.py
@@ -19,6 +19,11 @@ def board():
  print(line_2)
  print(line_3)
 
+board()
+
+def main():
+    print("Congratulations, you won the game!")
+
 def playerTurn():
     i = 0
     turn = 1
@@ -26,59 +31,163 @@ def playerTurn():
         if turn%2 == 1:
             playerOne = int(input("x's turn to choose a square (1-9):"))
             if playerOne == line_1[0]:
-                line_1[0] = X
+                line_1[0] = 'X'
+                if line_1[0] =='X' and line_2[0] =='X' and line_3[0] == 'X':
+                    main()
+                    break
+                elif line_1[0] =='X' and line_1[1] == 'X' and line_3[2] == 'X':
+                    main()
+                    break
+                elif line_1[0] == 'X' and line_2[1] == 'X' and line_3[2] == 'X':
+                    main()
+                    break
             elif playerOne == line_1[1]:
-                line_1[1] = X
+                line_1[1] = 'X'
+                if line_1[1] =='X' and line_2[1] =='X' and line_3[1] == 'X':
+                    main()
+                    break
+                elif line_1[0] =='X' and line_1[1] == 'X' and line_3[2] == 'X':
+                    main()
+                    break
             elif playerOne == line_1[2]:
-                line_1[2] = X
+                line_1[2] = 'X'
+                if line_1[2] =='X' and line_2[2] =='X' and line_3[2] == 'X':
+                    main()
+                    break
+                elif line_3[0] =='X' and line_2[1] == 'X' and line_1[2] == 'X':
+                    main()
+                    break
             elif playerOne == line_2[0]:
-                line_2[0] = X
+                line_2[0] = 'X'
+                if line_1[0] =='X' and line_2[0] =='X' and line_3[0] == 'X':
+                    main()
+                    break
+                elif line_2[0] =='X' and line_2[1] == 'X' and line_2[2] =='X':
+                   main()
+                   break
             elif playerOne == line_2[1]:
-                line_2[1] = X
+                line_2[1] = 'X'
+                if line_1[1] =='X' and line_2[1] =='X' and line_3[1] == 'X':
+                    main()
+                    break
+                elif line_2[0] =='X' and line_2[1] == 'X' and line_2[2] =='X':
+                    main()
+                elif line_3[0] =='X' and line_2[1] == 'X' and line_1[2] == 'X':
+                    main()
+                    break 
+                elif line_1[0] == 'X' and line_2[1] == 'X' and line_3[2] == 'X':
+                    main()
+                    break  
             elif playerOne == line_2[2]:
-                line_2[2] = X
+                line_2[2] = 'X'
+                if line_1[2] =='X' and line_2[2] =='X' and line_3[2] == 'X':
+                    main()
+                    break
+                elif line_2[0] =='X' and line_2[1] == 'X' and line_2[2] =='X':
+                    main()
+                    break
             elif playerOne == line_3[0]:
-                line_3[0] = X
+                line_3[0] = 'X'
+                if line_1[0] =='X' and line_2[0] =='X' and line_3[0] == 'X':
+                    main()
+                    break
+                elif line_3[0] =='X' and line_2[1] == 'X' and line_1[2] == 'X':
+                    main()
+                    break
             elif playerOne == line_3[1]:
-                line_3[1] = X
+                line_3[1] = 'X'
+                if line_1[1] =='X' and line_2[1] =='X' and line_3[1] == 'X':
+                    main()
+                    break
+                elif line_3[0] == 'X' and line_3[1] == 'X' and line_3[2] == 'X':
+                    main()
+                    break
             elif playerOne == line_3[2]:
-                line_3[2] = X
+                line_3[2] = 'X'
+                if line_1[2] =='X' and line_2[2] =='X' and line_3[2] == 'X':
+                    main()
+                    break
+                elif line_3[0] == 'X' and line_3[1] == 'X' and line_3[2] == 'X':
+                    main()
+                    break
+                elif line_1[0] == 'X' and line_2[1] == 'X' and line_3[2] == 'X':
+                    main()
+                    break
             i+=1
             turn+=1
             board()
         elif turn%2 == 0:
-            playerTwo = input("o's turn to choose a square (1-9):")
+            playerTwo = int(input("o's turn to choose a square (1-9):"))
+            if playerTwo == line_1[0]:
+                line_1[0] = 'O'
+                if line_1[0] =='O' and line_2[0] =='O' and line_3[0] == 'O':
+                    main()
+                    break
+                elif line_1[0] =='O' and line_1[1] == 'O' and line_1[2] == 'O'
+                    main()
+                    break
+                elif line_1[0] == 'O' and line_2[1] == 'O' and line_3[2] == 'O':
+                    main()
+                    break
+            elif playerTwo == line_1[1]:
+                line_1[1] = 'O'
+                if line_1[1] =='O' and line_2[1] =='O' and line_3[1] == 'O':
+                    main()
+                    break
+                elif line_1[0] =='O' and line_1[1] == 'O' and line_1[2] == 'O'
+                    main()
+                    break
+            elif playerTwo == line_1[2]:
+                line_1[2] = 'O'
+            elif playerTwo == line_2[0]:
+                line_2[0] = 'O'
+            elif playerTwo == line_2[1]:
+                line_2[1] = 'O'
+            elif playerTwo == line_2[2]:
+                line_2[2] = 'O'
+            elif playerTwo == line_3[0]:
+                line_3[0] = 'O'
+            elif playerTwo == line_3[1]:
+                line_3[1] = 'O'
+            elif playerTwo == line_3[2]:
+                line_3[2] = 'O'
             i+=1
             turn+=1
             board()
 
 
+playerTurn()
+
 
     
-# def playerOne_Tic():
-#     if playerOne == line_1[0]:
-#         line_1[0] = 
-# board()
-# playerTurn()   
-
-
-# playerTwo = float(input("o's turn to choose a square (1-9):"))
-
-
-# def playerTwo_Tic():
-#  for x in line_1:
-#     if( playerTwo == x):
-#         line_1[i]= x
-#  for x in line_2:
-#     if( playerTwo == x):
-#         line_2[i] = x
-#  for x in line_3:
-#     if (playerTwo == x):
-#         line_3[i] = x
 
 
 
-
+# 
+#     if line_1[0] =='O' and line_2[0] =='O' and line_3[0] == 'O':
+#         main()
+#         break
+#     elif line_1[1] =='O' and line_2[1] =='O' and line_3[1] == 'O':
+#         main()
+#         break
+#     elif line_1[2] =='O' and line_2[2] =='O' and line_3[2] == 'O':
+#         main()
+#         break
+#     elif line_1[0] =='O' and line_1[1] == 'O' and line_1[2] == 'O'
+#         main()
+#         break
+# #     elif line_2[0] =='O' and line_2[1] == 'O' and line_2[2] =='O':
+# #         main()
+# #         break
+#       elif line_3[0] == 'O' and line_3[1] == 'O' and line_3[2] == 'O':
+#           main()
+#           break
+# #     elif line_3[0] =='O' and line_2[1] == 'O' and line_1[2] == 'O':
+# #         main()
+# #         break
+#     # elif line_1[0] == 'O' and line_2[1] == 'O' and line_3[2] == 'O':
+#     #     main()
+#     #     break
 
  
 
